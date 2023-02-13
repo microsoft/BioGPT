d239d7cff78ebb7871c610f6b923e022c2fabbe7
Kateryna Dubinina <ururruu@gmail.com>
2023-02-13 18:38:49 +0200

    copy code for processing

--- server/resources/run_biogpt.py
+++ server/resources/run_biogpt.py
@@ -1,6 +1,22 @@
+from fairseq.models.transformer_lm import TransformerLanguageModel
+from flask import request
 from flask_restful import Resource
 
 
 class RunBioGptApi(Resource):
     def post(self):
-        print('hello world')
+        question = request.json.get('question')
+        m = TransformerLanguageModel.from_pretrained(
+            "checkpoints/Pre-trained-BioGPT",
+            "checkpoint.pt",
+            "data",
+            tokenizer='moses',
+            bpe='fastbpe',
+            bpe_codes="data/bpecodes",
+            min_len=100,
+            max_len_b=1024)
+        # m.cuda()
+        src_tokens = m.encode(question)
+        generate = m.generate([src_tokens], beam=5)[0]
+        answer = m.decode(generate[0]["tokens"])
+        return jsonify({"answer": answer})
