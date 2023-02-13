6233d23f84be7f6d93cd30f41f3a3f5d84f4990f
dmason-sb <dmason@superbio.ai>
2023-02-13 16:00:34 +0000

    added basic script for pretrained-biogpt inference

--- pretrained_biogpt_infer.py
+++ pretrained_biogpt_infer.py
@@ -13,7 +13,9 @@ m = TransformerLanguageModel.from_pretrained(
         max_len_b=1024)
 
 #m.cuda()
+
 src_tokens = m.encode("COVID-19 is")
 generate = m.generate([src_tokens], beam=5)[0]
 output = m.decode(generate[0]["tokens"])
+
 print(output)
