fdb5b03d658a543bd4ad110b18f73d2b5a7f9dac
Kateryna Dubinina <ururruu@gmail.com>
2023-02-14 15:47:51 +0200

    Changes to dockerfile to run server (#4)

--- server/resources/run_biogpt.py
+++ server/resources/run_biogpt.py
@@ -15,7 +15,7 @@ class RunBioGptApi(Resource):
             bpe_codes="data/bpecodes",
             min_len=100,
             max_len_b=1024)
-        # m.cuda()
+        m.cuda()
         src_tokens = m.encode(question)
         generate = m.generate([src_tokens], beam=5)[0]
         answer = m.decode(generate[0]["tokens"])
